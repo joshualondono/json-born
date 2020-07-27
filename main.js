de84d5aded4094e200c53b24a437ad8ec027b069
joshua londono <joshualondono@outlook.com>
2020-07-27 12:06:44 -0400

    didnt know how to write files and load, now I got it

--- main.js
+++ main.js
@@ -1,3 +1,41 @@
+const fs = require('fs');
+
+
+const getStudentByIndex = function(index){
+
+  const file = fs.readFileSync('../students.json', 'utf8')
+  const data = JSON.parse(file)
+
+  return data['students'][index]
+}
+
+const getStudentByName = function(str){
+
+  const file = fs.readFileSync('../students.json', 'utf8')
+  const data = JSON.parse(file)
+
+for(const name of data.students){
+  if(name.name === str){
+    return name
+  }
+} return -1
+}
+
+const graduateStudent = function(str){
+
+  const file = fs.readFileSync('../students.json', 'utf8')
+  const data = JSON.parse(file)
+
+  for(const name of data.students){
+    if(name.name === str){
+     name.term = name.term + 1
+
+   }
+
+  }
+  const newContents = JSON.stringify(data)
+  fs.writeFileSync('../students.json', newContents)
+}
 
 
 
